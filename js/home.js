40773c1e327afaff7abe91f57e1cf87f4314736c
Riyad Ahomed <mdriyadahmed5052@gmail.com>
2024-11-24 18:57:00 +0600

    money event

--- js/home.js
+++ js/home.js
@@ -1,1 +1,22 @@
-console.log('home added');
+// add money to the account
+/* 
+s-1:add event handler
+prevent page reload after submit
+s-2:get money to be added to the account balance
+
+*/
+
+//step-1:add an event handler to the add money button inside the from
+
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+    //prevent page reload after from submit
+    event.preventDefault();
+
+ //step-2:get money to be added to the account 
+const addMoneyInput  = document.getElementById('input-add-money').value;
+console.log(addMoneyInput);
+
+//get the pin number provided
+const pinNumberInput = document.getElementById('input-pin-number').value;
+console.log(pinNumberInput);
+})
